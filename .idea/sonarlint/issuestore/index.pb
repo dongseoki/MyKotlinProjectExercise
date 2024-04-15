ec78e75cc48b8b825faddac8ca3fadc147640177
Dong Seok Lee <dongseok.lee.log@gmail.com>
2024-04-15 19:49:07 +0900

    commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+;
+gradlew.bat,2/a/2a45a911a8f1836b0b6c5b758962572012d8f8c3
+@
+build.gradle.kts,d/b/dbcff70658daf80b53ce624f6adcaa529df5ed8d
+i
+9src/test/kotlin/essentials/conditions/PrettytimeKtTest.kt,3/2/32360c05fe5a252532ec15d271874e106e29fa89
+c
+3src/main/kotlin/essentials/conditions/prettytime.kt,2/4/24c6a10d70791b0eb322d0c88c2d97012f3d07ed
---
+++ src/main/kotlin/essentials/conditions/prettytime.kt
@@ -0,0 +1,31 @@
+package essentials.conditions.prettytime
+
+
+fun secondsToPrettyTime(seconds: Int): String {
+    when {
+        seconds < 0 -> return "Invalid input"
+        seconds == 0 -> return "Now"
+        else -> {
+            val hours = seconds / 3600
+            val minutes = (seconds % 3600) / 60
+            val sec = seconds % 60
+            val time = mutableListOf<String>()
+            if (hours > 0) time.add("$hours h")
+            if (minutes > 0) time.add("$minutes min")
+            if (sec > 0) time.add("$sec sec")
+            return time.joinToString(" ")
+        }
+    }
+    return ""
+}
+
+fun main() {
+    println(secondsToPrettyTime(-1)) // Invalid input
+    println(secondsToPrettyTime(0)) // Now
+    println(secondsToPrettyTime(45)) // 45 sec
+    println(secondsToPrettyTime(60)) // 1 min
+    println(secondsToPrettyTime(150)) // 2 min 30 sec
+    println(secondsToPrettyTime(1410)) // 23 min 30 sec
+    println(secondsToPrettyTime(60 * 60)) // 1 h
+    println(secondsToPrettyTime(3665)) // 1 h 1 min 5 sec
+}
